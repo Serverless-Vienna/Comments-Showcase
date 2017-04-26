e531d7e7c739bf7b5d4ef67e6a59666b6378846a
László Király <laszlo.t.kiraly@gmail.com>
2017-04-26 07:59:16 +0200

    refactoring message to comment

---
+++ src/CommentList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import sanitizeHtml from 'sanitize-html';
+
+class CommentList extends React.Component {
+
+  render() {
+    if (this.props.list.length === 0) {
+      return <div>No comments found.</div>;
+    } else {
+      return (
+        <div className="CommentList">
+          <ul>
+            {/* TODO: use key from item, e.g. client uuid ( + userid or server uuid ? ), also needs a mechanism to deal with duplicate clientIds */}
+            {this.props.list.map(function (item, index) {
+              var serverTimeFormatted = item.serverTime !== undefined ? new Date(item.serverTime).toLocaleString() : "No server time";
+              let sanitizedContent = sanitizeHtml(item.value, {
+                allowedTags: [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
+                'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div', 'ins', 'pre' ]
+                });
+              return <li key={item.uuid}>
+                <span className="key">from <b>{item.sender}</b> (@{serverTimeFormatted}):</span><br/>
+                <span className="value" dangerouslySetInnerHTML={{__html: sanitizedContent}}></span>
+              </li>;
+            })}
+          </ul>
+        </div>
+      )
+    }
+  }
+}
+
+export default CommentList;
